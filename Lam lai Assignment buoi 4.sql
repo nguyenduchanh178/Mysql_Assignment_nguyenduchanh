11d9d01ab6531394aa0b5bb5f0a184aa68744428
nguyenduchanh178 <119336083+nguyenduchanh178@users.noreply.github.com>
2022-12-07 00:25:52 +0900

    Add files via upload

---
+++ Lam lai Assignment buoi 4.sql
@@ -0,0 +1,98 @@
+USE testing_system;
+
+SELECT * 
+FROM `account` a
+JOIN department d ON d.departmentid= a.DepartmentID;
+
+SELECT * 
+FROM `account` 
+WHERE createddate>'2010-12-20';
+
+SELECT *
+FROM `account` a
+JOIN department d ON d.DepartmentID= a.DepartmentID
+WHERE departmentname = 'phong dev 1' Or 'phong dev 2';
+
+SELECT d.departmentname, COUNT(a.departmentid) as totalMember
+FROM `account` a
+JOIN department d ON d.DepartmentID= a.DepartmentID 
+GROUP BY d.departmentid
+HAVING totalmember >3;
+
+SELECT content, COUNT(e.questionid) as totalquestion
+FROM `question` a
+JOIN examquestion e ON e.questionid = a.questionid
+GROUP BY e.questionid
+HAVING totalquestion = (SELECT MAX(totalquestion) FROM (SELECT COUNT(e.questionid) as totalquestion
+                                                         FROM `question` a
+                                                         JOIN examquestion e ON e.questionid = a.questionid
+                                                         GROUP BY e.questionid ) as tempt_table);
+
+
+SELECT c.Categoryname, COUNT(q.content)
+FROM categoryquestion c
+JOIN question q ON c.categoryid= q.CategoryID
+GROUP BY q.categoryid;
+
+SELECT q.content, COUNT(e.examid)
+FROM question q 
+JOIN examquestion e ON q.questionid = e.QuestionID
+GROUP BY e.questionid;
+
+SELECT q.content, COUNT(a.answerid)
+FROM question q
+JOIN answer a ON a.questionid=q.questionid
+GROUP BY a.questionid;
+
+SELECT a.groupname, COUNT(g.groupid)
+FROM `group` a
+JOIN groupaccount g ON g.groupid= a.groupid
+GROUP BY g.groupid;
+
+SELECT p.positionname, COUNT(a.accountid) as totalmember
+FROM `account` a
+JOIN position p ON p.positionid= a.positionid
+GROUP BY a.positionid
+HAVING totalmember = (SELECT MIN(totalmember) FROM (SELECT COUNT(a.accountid) as totalmember
+												   FROM `account` a
+                                                   JOIN position p ON p.positionid= a.positionid
+												GROUP BY a.positionid) as totalmember2);
+
+SELECT d.departmentname, COUNT(a.accountid)
+FROM `account` a
+JOIN department d ON d.departmentid= a.departmentid
+GROUP BY a.departmentid;
+
+SELECT *
+FROM question q
+JOIN typequestion t ON t.typeid= q.typeid;
+ 
+
+SELECT t.typename, COUNT(q.typeid)
+FROM typequestion t
+JOIN question q ON q.typeid= t.typeid
+GROUP BY q.typeid;
+
+SELECT g.groupname, COUNT(a.groupid) as totalaccount
+FROM `group` g 
+JOIN groupaccount a ON a.groupid= g.groupid
+GROUP BY a.groupid
+HAVING totalaccount=0;
+
+SELECT q.content, COUNT(a.answerid) as totalanswer
+FROM question q
+JOIN answer a ON a.questionid=q.questionid
+GROUP BY a.questionid
+HAVING totalanswer = 0;
+
+
+
+
+
+ 
+
+
+
+
+
+
---
+++ làm lại buổi 3.sql
@@ -0,0 +1,61 @@
+USE testing_system;
+
+/--Question 2: lấy ra tất cả các phòng ban--/
+
+SELECT * FROM department;
+
+Question 3: lấy ra id của phòng ban "Sale"
+
+SELECT departmentid FROM department
+WHERE departmentname = 'phong sale';
+
+-- Question 4: lấy ra thông tin account có full name dài nhất
+
+SELECT *
+FROM `account`
+WHERE length(fullname)= (SELECT MAX(length(fullname)) FROM `account`);
+
+SELECT *
+FROM `account`
+WHERE length(fullname)= (SELECT MAX(length(fullname)) FROM `account`) AND departmentid=3;
+
+SELECT groupname
+FROM `group`
+WHERE createddate < '2019-12-20';
+
+SELECT examid
+FROM `exam` 
+WHERE duration >= 60 AND createddate < '2019-12-20';
+
+SELECT groupname
+FROM `group`
+ORDER BY createddate DESC
+LIMIT 5;
+
+SELECT COUNT(accountid)
+FROM `account` a
+JOIN department d ON d.departmentid= a.departmentid
+WHERE a.departmentid=2;
+
+SELECT fullname
+FROM `account`
+WHERE fullname LIKE 'D%o';
+
+UPDATE `account`
+SET fullname = 'Nguyễn Bá Lộc', email= 'loc.nguyenba@vti.com.vn'
+WHERE accountid=5 ;
+
+SELECT *, length(fullname)  as sokytu1   --  nguyễn    6    8
+FROM `account`
+Having length(fullname) = (select max(sokytu)
+					from  (sELECT length(fullname) as sokytu
+							FROM account) as t);
+
+select max(sokytu)
+from  (sELECT char_length(fullname) as sokytu
+FROM account) as t;
+
+
+
+
+
